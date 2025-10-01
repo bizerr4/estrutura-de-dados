d2565aa141cdb51e14c7bcaef9eb6818fd305706
Bizerra <claudiobizerra08@gmail.com>
2025-10-01 12:55:37 -0300

    Update listalinkada.py

--- linkedlista-atividade/listalinkada.py
+++ linkedlista-atividade/listalinkada.py
@@ -1,39 +1,9 @@
-class No:
-    def __init__(self, dado):
-        self.dado = dado
-        self.proximo = None
-
-class ListaLinkada:
-    def __init__(self):
-        self.cabeca = None
-
-    def inserir(self, dado):
-        novo_no = No(dado)
-        if self.cabeca is None:
-            self.cabeca = novo_no
-        else:
-            atual = self.cabeca
-            while atual.proximo:
-                atual = atual.proximo
-            atual.proximo = novo_no
-
-    def mostrar(self):
-        atual = self.cabeca
-        while atual:
-            print(atual.dado, end=" -> ")
-            atual = atual.proximo
-        print("None")
-
-    def remover(self, valor):
-        atual = self.cabeca
-        anterior = None
-        while atual and atual.dado != valor:
-            anterior = atual
-            atual = atual.proximo
-        if atual is None:
-            print("valor não encontrado")
-            return
-        if anterior is None:
-            self.cabeca = atual.proximo
-        else:
-            anterior.proximo = atual.proximo
+class Node:
+    def __init__ (self, val, next=None):
+        self.val = val
+        self.next = next
+    
+    def __str__ (self):
+        return str(self.val)
+    
+      
